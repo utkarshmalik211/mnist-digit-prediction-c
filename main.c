4fb4f7874c7b0efd33d389e218b2ddc422f9d54e
Utkarsh Malik <utkarshmalik211@gmail.com>
2017-09-04 15:21:38 +0530

    neural network completegit add .

--- main.c
+++ main.c
@@ -1,10 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <cv.h>
+#include <highgui.h>
+#include <math.h>
+
 #include "neural_net.h"
 #include "read_mnist.h"
+#include "train_net.h"
 
-void main(){
+void main(int argc, char *argv[]){
+	FILE *trainImageFile = openMNISTImageFile("data/train-images-idx3-ubyte");
+	FILE *trainLableFile = openMNISTImageFile("data/train-labels-idx1-ubyte");
+
+	FILE *testImageFile = openMNISTImageFile("data/t10k-images-idx3-ubyte");
+	FILE *testLableFile = openMNISTImageFile("data/t10k-labels-idx1-ubyte");
+
+	IplImage* img = 0; 
+  	int height,width,step,channels;
+  	uchar *data;
+ 
+	if(argc<2){
+	printf("Usage: main <image-file-name>\n\7");
+	exit(0);
+	}
+ 
+  	// load an image  
+  	img=cvLoadImage(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+  	if(!img){
+    	printf("Could not load image file: %s\n",argv[1]);
+    	exit(0);
+  	}
+
 	Network *a = createNetwork(748,20,10);
 	initNetwork(a,748,20,10);
 	initWeights(a,HIDDEN);
 	initWeights(a,OUTPUT);
-	printf("%d\n",a->inpNodeSize);
-}
+
+	for (int i = 0; i < 59990; i++)
+	{
+		MNIST_Image img = getImage(trainImageFile);
+		MNIST_Label lbl = getLabel(trainLableFile);
+		Vector *image = getVectorFromImage(&img);
+		feedInput(a,image);
+		feedForwardNetwork(a);
+		backPropagateNetwork(a,lbl);
+        // printf("\rIn progress %d", i/600);
+        printf("%d   %d\n",getNetworkClassification(a),lbl);
+	}
+	printf("\n");
+	for (int i = 2; i < 10; i++)
+	{
+		MNIST_Image img = getImage(testImageFile);
+		MNIST_Label lbl = getLabel(testLableFile);
+		Vector *image = getVectorFromImage(&img);
+		feedInput(a,image);
+		feedForwardNetwork(a);
+		// backPropagateNetwork(a,(int)lbl);
+        // printf("\rIn progress %d", i/600);
+		printf("%d   %d\n",getNetworkClassification(a),lbl);
+	}
+
+	IplImage* dst= cvCreateImage(cvSize( 28, 28), img->depth, img->nChannels );
+
+	cvResize(img, dst,CV_INTER_AREA );
+	img = dst;
+	// get the image data
+	height    = img->height;
+	width     = img->width;
+	step      = img->widthStep;
+	channels  = img->nChannels;
+	data      = (uchar *)img->imageData;
+	printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
+	// create a window
+	cvNamedWindow("mainWin", CV_WND_PROP_FULLSCREEN); 
+	cvMoveWindow("mainWin", 100, 100);
+	double b[28][28];
+	// invert the image
+	for(int i=0;i<height;i++){ 
+		for(int j=0;j<width;j++){ 
+			for(int k=0;k<channels;k++)
+				data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
+			b[i][j] = ((int) data[i*step+j*channels] == 0) ? 1:0;
+		}
+	} 
+	// for(i=0;i<28;i++){
+	//   for(j=0;j<28;j++){
+	//     printf("%d",(int)a[i][j]);
+	//   }
+	// }
+	Vector *image1 = (Vector*)malloc(sizeof(double)+(sizeof(double)*784));
+	image1->size = 28*28;
+	for(int i=0;i<28;i++){
+	  for(int j=0;j<28;j++){
+	   image1->vals[i*28 + j]=b[i][j];
+	  }
+	}
+	feedInput(a,image1);
+	feedForwardNetwork(a);
+	printf("%d\n",getNetworkClassification(a));
+	// show the image
+	cvShowImage("mainWin", img ); 
+	// wait for a key
+	cvWaitKey(0); 
+	// release the image
+	cvReleaseImage(&img );
+	printf("\n");
+
+	}
--- read_mnist.h
+++ read_mnist.h
@@ -1,21 +1,156 @@
+#define MNIST_TRAINING_SET_IMAGE_FILE_NAME "data/train-images-idx3-ubyte" 
+#define MNIST_TRAINING_SET_LABEL_FILE_NAME "data/train-labels-idx1-ubyte" 
+
+#define MNIST_TESTING_SET_IMAGE_FILE_NAME "data/t10k-images-idx3-ubyte"  
+#define MNIST_TESTING_SET_LABEL_FILE_NAME "data/t10k-labels-idx1-ubyte"  
+
+#define MNIST_MAX_TRAINING_IMAGES 60000                     
+#define MNIST_MAX_TESTING_IMAGES 10000                      
+#define MNIST_IMG_WIDTH 28                                  
+#define MNIST_IMG_HEIGHT 28                                 
+
+typedef struct MNIST_ImageFileHeader MNIST_ImageFileHeader;
+typedef struct MNIST_LabelFileHeader MNIST_LabelFileHeader;
+
+typedef struct MNIST_Image MNIST_Image;
 typedef uint8_t MNIST_Label;
 
 typedef struct MNIST_Image{
-	uint8_t pixel[28*28];
-} MNIST_Image;
+    uint8_t pixel[28*28];
+}  MNIST_Image;
+
+typedef struct MNIST_ImageFileHeader{
+    uint32_t magicNumber;
+    uint32_t maxImages;
+    uint32_t imgWidth;
+    uint32_t imgHeight;
+} MNIST_ImageFileHeader;
+
+typedef struct MNIST_LabelFileHeader{
+    uint32_t magicNumber;
+    uint32_t maxImages;
+} MNIST_LabelFileHeader;
 
 typedef struct Vector{
-	int size;
-	double vals[];
+    int size;
+    double vals[];
 } Vector;
 
-// convert mnist image structure to Vector structure
 Vector *getVectorFromImage(MNIST_Image *img){
-	
-	Vector *v = (Vector*)malloc(sizeof(Vector)+(28*28*sizeof(double)));
-	v->size = 28*28;
+    Vector *v = (Vector*)malloc(sizeof(Vector)+(28*28*sizeof(double)));
+    v->size = 28*28;
 
-	for(int i=0;i<v->size;i++)
-		v->vals[i] = img->pixel[i] ? 1 :0;
-	return v;
+    for(int i = 0 ; i <v->size;i++){
+        v->vals[i] = img->pixel[i] ? 1 : 0;
+    }
+    return v;
 }
+
+uint32_t flipBytes(uint32_t n){
+    
+    uint32_t b0,b1,b2,b3;
+    
+    b0 = (n & 0x000000ff) <<  24u;
+    b1 = (n & 0x0000ff00) <<   8u;
+    b2 = (n & 0x00ff0000) >>   8u;
+    b3 = (n & 0xff000000) >>  24u;
+    
+    return (b0 | b1 | b2 | b3);
+    
+}
+
+void readImageFileHeader(FILE *imageFile, MNIST_ImageFileHeader *ifh){
+    
+    ifh->magicNumber = 0;
+    ifh->maxImages   = 0;
+    ifh->imgWidth    = 0;
+    ifh->imgHeight   = 0;
+    
+    fread(&ifh->magicNumber, 4, 1, imageFile);
+    ifh->magicNumber = flipBytes(ifh->magicNumber);
+    
+    fread(&ifh->maxImages, 4, 1, imageFile);
+    ifh->maxImages = flipBytes(ifh->maxImages);
+    
+    fread(&ifh->imgWidth, 4, 1, imageFile);
+    ifh->imgWidth = flipBytes(ifh->imgWidth);
+    
+    fread(&ifh->imgHeight, 4, 1, imageFile);
+    ifh->imgHeight = flipBytes(ifh->imgHeight);
+}
+
+void readLabelFileHeader(FILE *imageFile, MNIST_LabelFileHeader *lfh){
+    
+    lfh->magicNumber =0;
+    lfh->maxImages   =0;
+    
+    fread(&lfh->magicNumber, 4, 1, imageFile);
+    lfh->magicNumber = flipBytes(lfh->magicNumber);
+    
+    fread(&lfh->maxImages, 4, 1, imageFile);
+    lfh->maxImages = flipBytes(lfh->maxImages);
+    
+}
+FILE *openMNISTImageFile(char *fileName){
+
+    FILE *imageFile;
+    imageFile = fopen (fileName, "rb");
+    if (imageFile == NULL) {
+        printf("Abort! Could not fine MNIST IMAGE file: %s\n",fileName);
+        exit(0);
+    }
+
+    MNIST_ImageFileHeader imageFileHeader;
+    readImageFileHeader(imageFile, &imageFileHeader);
+    
+    return imageFile;
+}
+
+FILE *openMNISTLabelFile(char *fileName){
+    
+    FILE *labelFile;
+    labelFile = fopen (fileName, "rb");
+    if (labelFile == NULL) {
+        printf("Abort! Could not find MNIST LABEL file: %s\n",fileName);
+        exit(0);
+    }
+
+    MNIST_LabelFileHeader labelFileHeader;
+    readLabelFileHeader(labelFile, &labelFileHeader);
+    
+    return labelFile;
+}
+
+/* Returns the next image in the given MNIST image file
+ */
+
+MNIST_Image getImage(FILE *imageFile){
+    
+    MNIST_Image img;
+    size_t result;
+    result = fread(&img, sizeof(img), 1, imageFile);
+    if (result!=1) {
+        printf("\nError when reading IMAGE file! Abort!\n");
+        exit(1);
+    }
+    
+    return img;
+}
+
+/*Returns the next label in the given MNIST label file
+ */
+
+MNIST_Label getLabel(FILE *labelFile){
+    
+    MNIST_Label lbl;
+    size_t result;
+    result = fread(&lbl, sizeof(lbl), 1, labelFile);
+    if (result!=1) {
+        printf("\nError when reading LABEL file! Abort!\n");
+        exit(1);
+    }
+    
+    return lbl;
+}
+
+
