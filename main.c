9233fa9ea54f5e5c4c5c9d2ed5c04998cdbf4fe5
Utkarsh Malik <utkarshmalik211@gmail.com>
2017-09-07 22:59:35 +0530

    added some images and image processing code Neural Networks disabled currently :`(

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ void main(int argc, char *argv[]){
 	exit(0);
 	}
 
-  	load an image
+  	// load an image
   	img=cvLoadImage(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
   	if(!img){
     	printf("Could not load image file: %s\n",argv[1]);
@@ -38,32 +38,31 @@ void main(int argc, char *argv[]){
 	initWeights(a,HIDDEN);
 	initWeights(a,OUTPUT);
 
-	for (int i = 0; i < 59990; i++)
-	{
-		MNIST_Image img = getImage(trainImageFile);
-		MNIST_Label lbl = getLabel(trainLableFile);
-		Vector *image = getVectorFromImage(&img);
-		feedInput(a,image);
-		feedForwardNetwork(a);
-		backPropagateNetwork(a,lbl);
-        // printf("\rIn progress %d", i/600);
-        printf("%d   %d\n",getNetworkClassification(a),lbl);
-	}
-	printf("\n");
-	for (int i = 0; i < 20; i++)
-	{
-		MNIST_Image img = getImage(testImageFile);
-		MNIST_Label lbl = getLabel(testLableFile);
-		Vector *image = getVectorFromImage(&img);
-		feedInput(a,image);
-		feedForwardNetwork(a);
-		// backPropagateNetwork(a,(int)lbl);
-        // printf("\rIn progress %d", i/600);
-		printf("%d   %d\n",getNetworkClassification(a),lbl);
-	}
-
+	// for (int i = 0; i < 59990; i++)
+	// {
+	// 	MNIST_Image img = getImage(trainImageFile);
+	// 	MNIST_Label lbl = getLabel(trainLableFile);
+	// 	Vector *image = getVectorFromImage(&img);
+	// 	feedInput(a,image);
+	// 	feedForwardNetwork(a);
+	// 	backPropagateNetwork(a,lbl);
+  //       // printf("\rIn progress %d", i/600);
+  //       printf("%d   %d\n",getNetworkClassification(a),lbl);
+	// }
+	// printf("\n");
+	// for (int i = 0; i < 20; i++)
+	// {
+	// 	MNIST_Image img = getImage(testImageFile);
+	// 	MNIST_Label lbl = getLabel(testLableFile);
+	// 	Vector *image = getVectorFromImage(&img);
+	// 	feedInput(a,image);
+	// 	feedForwardNetwork(a)
+	// 	// backPropagateNetwork(a,(int)lbl);
+  //       // printf("\rIn progress %d", i/600);
+	// 	printf("%d   %d\n",getNetworkClassification(a),lbl);
+	// }
+	double t = cvThreshold(img,img,128,255,0);
 	IplImage* dst= cvCreateImage(cvSize( 28, 28), img->depth, img->nChannels );
-
 	cvResize(img, dst,CV_INTER_AREA );
 	img = dst;
 	// get the image data
@@ -76,30 +75,31 @@ void main(int argc, char *argv[]){
 	// create a window
 	cvNamedWindow("mainWin", CV_WND_PROP_FULLSCREEN);
 	cvMoveWindow("mainWin", 100, 100);
-	double b[28][28];
+	int b[28*28],c[28][28];
 	// invert the image
 	for(int i=0;i<height;i++){
-		for(int j=0;j<width;j++){
-			for(int k=0;k<channels;k++)
-				data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
-			b[i][j] = ((int) data[i*step+j*channels] == 0) ? 1:0;
-		}
-	}
-	// for(i=0;i<28;i++){
-	//   for(j=0;j<28;j++){
-	//     printf("%d",(int)a[i][j]);
-	//   }
-	// }
-	Vector *image1 = (Vector*)malloc(sizeof(double)+(sizeof(double)*784));
-	image1->size = 28*28;
-	for(int i=0;i<28;i++){
-	  for(int j=0;j<28;j++){
-	   image1->vals[i*28 + j]=b[i][j];
+	  for(int j=0;j<width;j++){
+	    for(int k=0;k<channels;k++){
+	      data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
+	      b[i*28 + j] = c[i][j] = (data[i*step+j*channels+k]) ? 1:0 ;
+			}
 	  }
 	}
-	feedInput(a,image1);
-	feedForwardNetwork(a);
-	printf("%d\n",getNetworkClassification(a));
+	Vector *image1 = (Vector*)malloc(sizeof(double)+(sizeof(double)*784));
+	image1->size = 28*28;
+	for(int j=0;j<image1->size;j++){
+	   image1->vals[j]=b[j];
+	  }
+	for(int j=0;j<image1->size;j++){
+		printf("%d",(int)image1->vals[j]);
+		 if(j%28==0){
+			if(j!=0)
+		 		printf("\n");
+		}
+	}
+	// feedInput(a,image1);
+	// feedForwardNetwork(a);
+	// printf("%d\n",getNetworkClassification(a));
 	// show the image
 	cvShowImage("mainWin", img );
 	// wait for a key
